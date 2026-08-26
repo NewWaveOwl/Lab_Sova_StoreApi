f30f9c168e9cc0e648cfe2bcea5d144c9cb5956e
Sova, Artem <2272422@johnabbottcollege.net>
2026-08-26 09:31:36 -0400

    Lab 00 Step 3: Define product DTOs (AB#1001)

---
+++ DTOs/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab00_Sova.DTOs
+{
+public sealed record ProductDto(Guid Id, string Name, decimal Price, string Category);
+public sealed record CreateProductRequest(string Name, decimal Price, string Category);
+public sealed record UpdateProductRequest(string Name, decimal Price, string Category);
+
+
+
+}
