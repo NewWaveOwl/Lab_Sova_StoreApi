f3cbd6d325cf93aabb520505f0e75066a1b9d921
Sova, Artem <2272422@johnabbottcollege.net>
2026-08-26 09:50:13 -0400

    Lab 02 Step 1: Add DTO mapping extensions (AB#1001)

---
+++ Controllers/DtoMappings.cs
@@ -0,0 +1,24 @@
+﻿using Lab00_Sova.DTOs;
+using Lab00_Sova.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab00_Sova.Controllers
+{
+    public static class DtoMappings
+    {
+        public static ProductDto ToDto(this Product product) =>
+            new(product.Id, product.Name, product.Price, product.Category);
+
+        public static OrderDto ToDto(this Order order) =>
+            new(
+                order.Id,
+                order.CustomerName,
+                order.Status,
+                order.CreatedAt,
+                order.Items.Select(item => new OrderItemDto(
+                    item.ProductId, item.Quantity, item.UnitPrice)).ToList());
+    }
+
+
+}
