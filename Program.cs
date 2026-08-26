29904aa195caf207fdbd1bb4f216ef7918e9ddbc
Sova, Artem <2272422@johnabbottcollege.net>
2026-08-26 11:03:19 -0400

    Addes swagger to main Programm.cs

--- Program.cs
+++ Program.cs
@@ -15,12 +15,17 @@ builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+        options.SwaggerEndpoint("/openapi/v1.json", "Products API v1"));
+
 }
 
 app.UseHttpsRedirection();
