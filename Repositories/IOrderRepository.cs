19006f5becaa5fdb9eb2267ef5421b10a997f785
Sova, Artem <2272422@johnabbottcollege.net>
2026-08-26 09:42:03 -0400

    Lab 01 Step 1: Define repository interfaces (AB#1001)

---
+++ Repositories/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lab00_Sova.Models;
+
+namespace Lab00_Sova.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GetAllAsync();
+        Task<Order?> GetByIdAsync(Guid id);
+        Task AddAsync(Order order);
+    }
+}
---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Lab00_Sova.Models;
+
+namespace Lab00_Sova.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(Guid id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
